6169aa973262c429e74b15a588b2200b7701f9ac
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-07 20:50:54 -0500

    Agregando metodo para sumar dos lista por sus indices

--- src/main/java/co/com/sofka/App.java
+++ src/main/java/co/com/sofka/App.java
@@ -1,13 +1,27 @@
 package co.com.sofka;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class App {
+
+    private static final Logger logger = Logger.getLogger("MyLog");
+
+    public void sum(List<Integer> arr1, List<Integer> arr2) {
+        List<Integer> sum = IntStream.range(0, arr1.size())
+                .mapToObj(i -> arr1.get(i) + arr2.get(i))
+                .collect(Collectors.toList());
+        logger.log(Level.INFO, "La suma de cada indice de las dos listas es: {0}", sum);
+    }
+
+    public static void main(String[] args) {
+        final List<Integer> arr1 = List.of(3, 5, 7, 8);
+        final List<Integer> arr2 = List.of(0, 9, 6, 1, 4);
+        App app = new App();
+        app.sum(arr1, arr2);
     }
 }
