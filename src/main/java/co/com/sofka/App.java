e3bf4730825fc333e0f829d71a7ed92136b07090
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-07 20:58:50 -0500

    Agregando documentacion de la clase y metodos

--- src/main/java/co/com/sofka/App.java
+++ src/main/java/co/com/sofka/App.java
@@ -1,16 +1,27 @@
 package co.com.sofka;
 
-
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Clase principal para las operaciones matemáticas
+ *
+ * @author Ricardo Ortega <tattortega.28@gmail.com>
+ * @version 1.0.0 2022-07-07
+ * @since 1.0.0
+ */
 public class App {
 
     private static final Logger logger = Logger.getLogger("MyLog");
 
+    /**
+     * Método sumar, se recorre cada lista por su índice y se suma.
+     * @param arr1 List
+     * @param arr2 List
+     */
     public void sum(List<Integer> arr1, List<Integer> arr2) {
         List<Integer> sum = IntStream.range(0, arr1.size())
                 .mapToObj(i -> arr1.get(i) + arr2.get(i))
@@ -18,6 +29,11 @@ public class App {
         logger.log(Level.INFO, "La suma de cada indice de las dos listas es: {0}", sum);
     }
 
+    /**
+     * Método resta, se recorre cada lista por su índice y se resta.
+     * @param arr1 List
+     * @param arr2 List
+     */
     public void subtract(List<Integer> arr1, List<Integer> arr2) {
         List<Integer> subtract = IntStream.range(0, arr1.size())
                 .mapToObj(i -> arr1.get(i) - arr2.get(i))
@@ -25,6 +41,11 @@ public class App {
         logger.log(Level.INFO, "La resta de cada indice de las dos listas es: {0}", subtract);
     }
 
+    /**
+     * Método multiplicación, se recorre cada lista por su índice y se multiplica.
+     * @param arr1 List
+     * @param arr2 List
+     */
     public void multiplication(List<Integer> arr1, List<Integer> arr2) {
         List<Integer> multiplication = IntStream.range(0, arr1.size())
                 .mapToObj(i -> arr1.get(i) * arr2.get(i))
@@ -32,6 +53,11 @@ public class App {
         logger.log(Level.INFO, "La multiplicación de cada indice de las dos listas es: {0}", multiplication);
     }
 
+    /**
+     * Método división, se recorre cada lista por su índice, validando que el dividendo no sea 0 y se divide.
+     * @param arr1 List
+     * @param arr2 List
+     */
     public void division(List<Integer> arr1, List<Integer> arr2) {
         List<Integer> division = IntStream.range(0, arr1.size())
                 .filter(num -> arr2.get(num) != 0)
@@ -40,6 +66,11 @@ public class App {
         logger.log(Level.INFO, "La division de cada indice de las dos listas es: {0}", division);
     }
 
+    /**
+     * Método módulo, se recorre cada lista por su índice, validando que el dividendo no sea 0 y se saca el módulo.
+     * @param arr1 List
+     * @param arr2 List
+     */
     public void module(List<Integer> arr1, List<Integer> arr2) {
         List<Integer> module = IntStream.range(0, arr1.size())
                 .filter(num -> arr2.get(num) != 0)
